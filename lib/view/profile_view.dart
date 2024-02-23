105268e70e2e5ce09e1c87dad09c184e7380394b
amr2siji <160729120+amr2siji@users.noreply.github.com>
2024-02-23 21:59:41 +0530

    Initial Code Created

--- lib/view/profile_view.dart
+++ lib/view/profile_view.dart
@@ -19,11 +19,141 @@ class _ProfileViewState extends State<ProfileView> {
       home: Scaffold(
         backgroundColor: Colors.black54,
 
+        appBar: AppBar(
+          backgroundColor: Colors.black54,
+        ),
+
         body: Column(
+          // mainAxisAlignment:MainAxisAlignment.spaceBetween ,
+          // profile pic view
           children: [
-            Image.asset("assets/profileImg.jpg")
+            Center(
+              child: ClipOval(
+                child: Image.asset("assets/profileImg.jpg",height: 150, scale: 2, )
+            )
+            ),
+
+            SizedBox(
+              height: 20,
+            ),
+
+            Text(" Name ",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.w500)
+              ),
+
+            //profile Details
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                height: 350,
+                width: 360,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.blueGrey,
+
+                ),
+
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        " Name ",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500
+                        ),
+                      ),
+                      Text(
+                        " Ammar Ahamed ",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500
+                        ),
+                      ),
+
+                      SizedBox(
+                        height: 20,
+                      ),
+
+                      Text(
+                        " Email ",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500
+                        ),
+                      ),
+
+                      Text(
+                        " amr2siji@gmail.com ",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500
+                        ),
+                      ),
+
+                      SizedBox(
+                        height: 20,
+                      ),
+
+                      Text(
+                        " Mobile Number ",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500
+                        ),
+                      ),
+
+
+                      Text(
+                        " 0768391956 ",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500
+                        ),
+                      ),
+
+                      SizedBox(
+                        height: 20,
+                      ),
+
+                      Text(
+                        " Date of Birth ",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500
+                        ),
+                      ),
+
+                      Text(
+                        " 2001.08.27 ",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ),
+
+
+            )
 
           ],
+
         ),
       ),
 
